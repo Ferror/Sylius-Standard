029b42a5b6e6861dc898cea2e0dc353a66452e6c
Ferror <zmalcherczyk@gmail.com>
2022-07-05 19:48:03 +0200

    Dont run migrations by all containers

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -7,23 +7,25 @@ if [ "${1#-}" != "$1" ]; then
 fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
-	mkdir -p var/cache var/log public/media
+	mkdir -p var/cache var/log var/sessions public/media
 	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var public/media
 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var public/media
 
-	if [ "$APP_ENV" != 'prod' ]; then
-		composer install --prefer-dist --no-progress --no-interaction
-		bin/console assets:install --no-interaction
-		bin/console sylius:theme:assets:install public --no-interaction
-	fi
+# This is in the Dockerfile
+#	if [ "$APP_ENV" != 'prod' ]; then
+#		composer install --prefer-dist --no-progress --no-interaction --no-dev
+#		bin/console assets:install --no-interaction
+#		bin/console sylius:theme:assets:install public --no-interaction
+#	fi
 
 	until bin/console doctrine:query:sql "select 1" >/dev/null 2>&1; do
 	    (>&2 echo "Waiting for MySQL to be ready...")
 		sleep 1
 	done
 
-    bin/console doctrine:migrations:migrate --no-interaction
-    bin/console sylius:fixtures:load --no-interaction
+# THESE SHOULD BE RUN BY ONLY ONE SERVICE
+#    bin/console doctrine:migrations:migrate --no-interaction
+#    bin/console sylius:fixtures:load --no-interaction
 fi
 
 exec docker-php-entrypoint "$@"
